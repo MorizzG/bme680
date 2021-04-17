90d19519c4b68710127e78dd94817a097e38fa29
Moritz Gmeiner <moritz@gmeiner.net>
2021-04-17 18:39:34 +0200

    fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import bme680
+import constants
 import time
 
 print(
@@ -12,20 +13,18 @@ Press Ctrl+C to exit!
 """
 )
 
-try:
-    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-except IOError:
-    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+    sensor = bme680.BME680()
 
 # These oversampling settings can be tweaked to
 # change the balance between accuracy and noise in
 # the data.
 
-sensor.set_humidity_oversample(bme680.OS_2X)
-sensor.set_pressure_oversample(bme680.OS_4X)
-sensor.set_temperature_oversample(bme680.OS_8X)
-sensor.set_filter(bme680.FILTER_SIZE_3)
-sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+sensor.set_humidity_oversample(constants.OS_2X)
+sensor.set_pressure_oversample(constants.OS_4X)
+sensor.set_temperature_oversample(constants.OS_8X)
+sensor.set_filter(constants.FILTER_SIZE_3)
+sensor.set_gas_status(constants.ENABLE_GAS_MEAS)
 
 sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(150)
